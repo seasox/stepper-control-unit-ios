dcf5409fd1cb437009843788d3c687ff110d4a69
Jeremy Boy <github@jboy.eu>
2021-03-06 00:34:46 +0100

    Initial Commit

---
+++ Shared/StepperControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepperControlApp.swift
+//  Shared
+//
+//  Created by Jeremy Boy on 06.03.21.
+//
+
+import SwiftUI
+
+@main
+struct StepperControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
