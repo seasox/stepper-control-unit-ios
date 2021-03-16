be53b941f7a9f8447ed73113b5e880efa357a5a7
Jeremy Boy <github@jboy.eu>
2021-03-16 22:07:21 +0100

    Add PhotoModeView, ConnectView

---
+++ Shared/Command/ConnectView.swift
@@ -0,0 +1,20 @@
+//
+//  ConnectView.swift
+//  StepperControl
+//
+//  Created by Jeremy Boy on 16.03.21.
+//
+
+import SwiftUI
+
+struct ConnectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ConnectView_Previews: PreviewProvider {
+    static var previews: some View {
+        ConnectView()
+    }
+}
---
+++ Shared/Command/PhotoModeView.swift
@@ -0,0 +1,20 @@
+//
+//  PhotoModeView.swift
+//  StepperControl
+//
+//  Created by Jeremy Boy on 16.03.21.
+//
+
+import SwiftUI
+
+struct PhotoModeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PhotoModeView_Previews: PreviewProvider {
+    static var previews: some View {
+        PhotoModeView()
+    }
+}
